e4da5680a9886847ddb31dcaf1edcc7270bd0e7a
LAPTOP-8BL927LI\kartt <karttriin@gmail.com>
2024-09-03 17:16:11 +0300

    Planetslnit

---
+++ SciCalc/SciCalc/app.xaml.cs
@@ -0,0 +1,12 @@
+namespace MauiPlanets
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
